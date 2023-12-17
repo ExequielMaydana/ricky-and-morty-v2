d5b83528017bdd75516e9e3fd0870b5e253faef8
ExequielMaydana <exedevcoding22@gmail.com>
2023-12-16 23:42:31 -0300

    edit header

--- src/components/header/LocationCard.jsx
+++ src/components/header/LocationCard.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import './style/styleHeader.css'
+import React from "react";
+import "./style/styleHeader.css";
 
-const LocationCard = ({location}) => {
+const LocationCard = ({ location }) => {
+  const date = new Date(location?.created);
+  const dateFormatted = date.toLocaleString();
 
-
-    const date = new Date(location?.created)
-    const dateFormatted = date.toLocaleString()
-    
   return (
-    <article className='card-location'>
-        <section className='location-text'>
-            <h1 className='text-title'>{location?.name}</h1>
-            <p className='text-date'><b>Created: </b>{dateFormatted}</p>
-        </section>
-        <section className='location-list-description'>
-            <ul className='description-container-items'>
-                <li className='item-description'><b>Type: </b>{location?.type}</li>
-                <li className='item-description'><b>Dimension: </b>{location?.dimension}</li>
-                <li className='item-description'><b>Populations: </b>{location?.residents.length}</li>
-            </ul>
-        </section>
+    <article className="card-location">
+      <section className="location-text">
+        <h1 className="text-title">{location?.name}</h1>
+        <p className="text-date">
+          <b>Creado: </b>
+          {dateFormatted}
+        </p>
+      </section>
+      <ul className="description-container-items">
+        <li className="item-description">
+          <b>Tipo: </b>
+          {location?.type}
+        </li>
+        <li className="item-description">
+          <b>Dimensión: </b>
+          {location?.dimension}
+        </li>
+        <li className="item-description">
+          <b>Poblaciones: </b>
+          {location?.residents.length}
+        </li>
+      </ul>
     </article>
-  )
-}
+  );
+};
 
-export default LocationCard
+export default LocationCard;
